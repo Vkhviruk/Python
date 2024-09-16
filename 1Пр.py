399dcf478af4edfca19e332958e9d233fdd86fe2
Vkhviruk <khvirukvictoria@gmail.com>
2024-09-16 20:13:28 +0300

    Add files via upload

---
+++ 1Пр.py
@@ -0,0 +1,12 @@
+a = "Hello would"
+name = "Victoria"
+lastname = "Khviruk"
+age = 16
+print("Значення змінної:", a)
+print("Тип змінної:", type(a))
+print("Значення змінної:", name)
+print("Тип змінної:", type(name))
+print("Значення змінної:", lastname)
+print("Тип змінної:", type(lastname))
+print("Значення змінної:", age)
+print("Тип змінної:", type(age))
