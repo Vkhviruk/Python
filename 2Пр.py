c30057973ed17ec98d84e95b610f5a9b60f221a6
Vkhviruk <khvirukvictoria@gmail.com>
2024-09-16 20:40:46 +0300

    Add files via upload

---
+++ 2Пр.py
@@ -0,0 +1,13 @@
+name = "Victoria"
+lastname = "Khviruk"
+age = 16
+if type(name) == type(lastname):
+    print(type(name))
+else:
+    print("Змінні мають різні типи даних")
+list = [name, lastname]
+print(" ".join(list))
+if type(age) == type(lastname):
+    print("type age = type lastname")
+if type(age) != type(lastname):
+    print("type 'age' ≠ type 'lastname'")
